2df5d0bc56ad3583be8f00420f7f8293dd4f5b4f
miketruax <mike@miketruax.com>
2016-08-21 21:56:11 -0500

    Fixes small errors and adds graphical name input

--- js/main.js
+++ js/main.js
@@ -17,11 +17,13 @@ I suppose you could also add each one as a .js file but I feel like that creates
 too many files and defeats the purpose of a single modularized .js file
 */
 !(function(){
-var $start = '<div class="screen screen-start" id="start"> <header> <h1>Tic Tac Toe</h1><a href="#" class="button computer">Play Against Computer</a><br><br><a href="#" class="button player">Play Against Player</a> </header> </div>';
+  var $start = '<div class="screen screen-start" id="start"><header><h1>Tic Tac Toe</h1><div class="againstWhom"><a href="#" class="button computer">Play Against Computer</a><br><br><a href="#" class="button player">Play Against Player</a></div>';
+  $start +=  '<div id="nameOne"><label for="playOne" class="play1 playInput">Player One\'s Name:</label><input type="text" name="playOne" id="playOne" class="playInput"></div>';
+  $start +=  '<div id="nameTwo"><label for="playTwo" class="play2  playInput">Player Two\'s Name:</label><input type="text" name="playTwo" id="playTwo" class="playInput"></div><div class="startName"><a href="#" class="button startName">Start</a></div></header></div>';
 var $win = '<div class="screen screen-win" id="finish"> <header> <h1>Tic Tac Toe</h1> <p class="message"></p> <a href="#" class="button">New game</a> </header></div>';
 var $board = '<div class="board" id="board"> <header> <h1>Tic Tac Toe</h1> <ul>';
-$board +='<li class="players player1"><svg xmlns="http://www.w3.org/2000/svg" width="42" height="42" viewBox="0 0 42 42" version="1.1"><g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g transform="translate(-200.000000, -60.000000)" fill="#000000"><g transform="translate(200.000000, 60.000000)"><path d="M21 36.6L21 36.6C29.6 36.6 36.6 29.6 36.6 21 36.6 12.4 29.6 5.4 21 5.4 12.4 5.4 5.4 12.4 5.4 21 5.4 29.6 12.4 36.6 21 36.6L21 36.6ZM21 42L21 42C9.4 42 0 32.6 0 21 0 9.4 9.4 0 21 0 32.6 0 42 9.4 42 21 42 32.6 32.6 42 21 42L21 42Z"/></g></g></g></svg></li>';
-$board +='<li class="players player2"><svg xmlns="http://www.w3.org/2000/svg" width="42" height="43" viewBox="0 0 42 43" version="1.1"><g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g transform="translate(-718.000000, -60.000000)" fill="#000000"><g transform="translate(739.500000, 81.500000) rotate(-45.000000) translate(-739.500000, -81.500000) translate(712.000000, 54.000000)"><path d="M30 30.1L30 52.5C30 53.6 29.1 54.5 28 54.5L25.5 54.5C24.4 54.5 23.5 53.6 23.5 52.5L23.5 30.1 2 30.1C0.9 30.1 0 29.2 0 28.1L0 25.6C0 24.5 0.9 23.6 2 23.6L23.5 23.6 23.5 2.1C23.5 1 24.4 0.1 25.5 0.1L28 0.1C29.1 0.1 30 1 30 2.1L30 23.6 52.4 23.6C53.5 23.6 54.4 24.5 54.4 25.6L54.4 28.1C54.4 29.2 53.5 30.1 52.4 30.1L30 30.1Z"/></g></g></g></svg></li>';
+$board +='<li class="players player1"><svg xmlns="http://www.w3.org/2000/svg" width="42" height="42" viewBox="0 0 42 42" version="1.1"><g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g transform="translate(-200.000000, -60.000000)" fill="#000000"><g transform="translate(200.000000, 60.000000)"><path d="M21 36.6L21 36.6C29.6 36.6 36.6 29.6 36.6 21 36.6 12.4 29.6 5.4 21 5.4 12.4 5.4 5.4 12.4 5.4 21 5.4 29.6 12.4 36.6 21 36.6L21 36.6ZM21 42L21 42C9.4 42 0 32.6 0 21 0 9.4 9.4 0 21 0 32.6 0 42 9.4 42 21 42 32.6 32.6 42 21 42L21 42Z"/></g></g></g></svg><span id="O"></span></li>';
+$board +='<li class="players player2"><svg xmlns="http://www.w3.org/2000/svg" width="42" height="43" viewBox="0 0 42 43" version="1.1"><g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g transform="translate(-718.000000, -60.000000)" fill="#000000"><g transform="translate(739.500000, 81.500000) rotate(-45.000000) translate(-739.500000, -81.500000) translate(712.000000, 54.000000)"><path d="M30 30.1L30 52.5C30 53.6 29.1 54.5 28 54.5L25.5 54.5C24.4 54.5 23.5 53.6 23.5 52.5L23.5 30.1 2 30.1C0.9 30.1 0 29.2 0 28.1L0 25.6C0 24.5 0.9 23.6 2 23.6L23.5 23.6 23.5 2.1C23.5 1 24.4 0.1 25.5 0.1L28 0.1C29.1 0.1 30 1 30 2.1L30 23.6 52.4 23.6C53.5 23.6 54.4 24.5 54.4 25.6L54.4 28.1C54.4 29.2 53.5 30.1 52.4 30.1L30 30.1Z"/></g></g></g></svg><span id="X"></span></li>';
 $board +='</ul> </header> <ul class="boxes"> <li class="box"></li> <li class="box"></li><li class="box"></li><li class="box"></li><li class="box"></li><li class="box"></li><li class="box"></li><li class="box"></li><li class="box"></li></ul></div>';
 var againstPlayer; //whether or not it's against player or AI
 var activePlayer; //active player
@@ -36,23 +38,34 @@ var toStart = function(){ //resets to the start page and adds event listeners
 
     $('.computer').on('click', function(){ //if computer clicked, will use AI for playing
       againstPlayer = false;
-      initialize(); //starts board
-      nameOne = prompt('Please enter Your Name:'); //prompts for only one player's name
-      nameTwo = 'SkyNet'; //computer is always named SkyNet... always watching...
+      $('.againstWhom').slideToggle('slow', function(){ //slides out buttons for selecting whom to play against
+        $('#nameOne').show('slow'); //slides in just field for name 1
+        $('.startName').show('slow'); //slides it start button
+        $('#playTwo').val('SkyNet'); //sets hidden text box to be name SkyNet... always watching....
+      });
+    });
+    $('.player').on('click', function(){ //if computer clicked, will use AI for playing
+      againstPlayer = true; //against player not AI
+      $('.againstWhom').slideToggle('slow', function(){
+        $('#nameOne').show('slow'); //slides in both player one and player two names
+        $('#nameTwo').show('slow');
+        $('.startName').show('slow'); //slides in start button
+        });
     });
 
-    $('.player').on('click',function(){ //allows two human players
+    $('.startName').on('click',function(){ //allows two human players
       initialize(); //starts board
-      againstPlayer = true;
-      nameOne = prompt('Please enter Player One\'s Name:'); //prompts for first players name
-      nameTwo = prompt('Please enter Player Two\'s Name:'); //prompts for second players name
     });
   });
 };
 
 function initialize(){
+  nameOne = $('#playOne').val(); //saves player 1 and 2s names
+  nameTwo = $('#playTwo').val();
+  $('body').html($board); //sets board up initially
   $('document').ready(function(){
-    $('body').html($board); //sets board up initially
+  $('#O').html(nameOne); //shows players names 1 and two
+  $('#X').html(nameTwo);
     $('.box').on('click', placeMove); //addsEventlisteners to ensure click funcionality works
     $('.player1').addClass('active'); //in case of restart
     $('.player2').removeClass('active'); //in case of restart resets starting player
@@ -81,6 +94,11 @@ function initialize(){
         $(this).addClass('box-filled-'+activePlayer);
         updateBoard($(this).index());//updates the board for both AI and win verification passes current index
         activePlayer === 1 ? activePlayer = 2 : activePlayer = 1;
+        //I was told the ternary operator could sometimes cause issues and to just be careful with them
+        // it causes an error message (incorrectly) in JSHint so I have left them in, but could you expand
+        //upon what issues they might cause as I find the syntax to be visually appealing and so I prefer them
+        // also it cuts down lines of code but less code doesn't always mean better coding so I was curious if I
+        //should stop using them or just not use them in specific cases.
 
       }
       if(activePlayer ===2 && !againstPlayer && checkWinner()===0){ //auto plays if against computer
@@ -90,36 +108,6 @@ function initialize(){
         endGame(checkWinner()); //ends game with winner value
       }
 
-    };
-
-  var computerMove = function(){
-    var highestScore = -10000; //used to compare potential best moves
-    var index; //to save index for later use
-
-    for(var i=0; i<availMoves.length; i++){
-      currentBoard[availMoves[i]] = 2; //makes a move for outcome testing
-      var score = evaluateTotalScore();
-      //compares potential play to base score of -10000 this is on the offchance all
-      //moves are bad. It will then pick the least bad of the bunch
-      if(score > highestScore){
-        highestScore = score; //if higher, this becomes the best play
-        index = i; //saves index to click box after all passes
-      }
-      currentBoard[availMoves[i]] = 0; //resets the move to test a different one
-    }
-     $('.boxes').children()[availMoves[index]].click(); //clicks the box that resulted in highest line scores
-
-    var evaluateTotalScore = function(){ //goes through line by line to get sum of score
-      var score = 0;
-      score += checkLine(0,1,2); //checks horizontal line
-      score += checkLine(3,4,5); //checks horizontal line
-      score += checkLine(6,7,8); //checks horizontal line
-      score += checkLine(0,3,6); //checks vertical line
-      score += checkLine(1,4,7); //checks vertical line
-      score += checkLine(2,5,8); //checks vertical line
-      score += checkLine(0,4,8); //checks diagonal line
-      score += checkLine(2,4,6); //checks diagonal line
-      return score; //returns score for comparison
     };
 
     /*This function goes through the three cells input from above
@@ -133,7 +121,7 @@ function initialize(){
     the line. I have yet to be able to beat it so that's a plus for the minMax
     and a huge minus for fun....
     */
-    function checkLine(a, b, c) {
+    var checkLine = function(a, b, c) {
       var lineScore = 0;
 
      if (currentBoard[a] == 2) { //starting with first in line
@@ -183,9 +171,42 @@ function initialize(){
         }
      }
      return lineScore; //if it wasn't already, return the score for this line
-  }
-
   };
+    var evaluateTotalScore = function(){ //goes through line by line to get sum of score
+      var score = 0;
+      score += checkLine(0,1,2); //checks horizontal line
+      score += checkLine(3,4,5); //checks horizontal line
+      score += checkLine(6,7,8); //checks horizontal line
+      score += checkLine(0,3,6); //checks vertical line
+      score += checkLine(1,4,7); //checks vertical line
+      score += checkLine(2,5,8); //checks vertical line
+      score += checkLine(0,4,8); //checks diagonal line
+      score += checkLine(2,4,6); //checks diagonal line
+      return score; //returns score for comparison
+    };
+
+  var computerMove = function(){
+    var highestScore = -10000; //used to compare potential best moves
+    var index; //to save index for later use
+
+    for(var i=0; i<availMoves.length; i++){
+      currentBoard[availMoves[i]] = 2; //makes a move for outcome testing
+      var score = evaluateTotalScore();
+      //compares potential play to base score of -10000 this is on the offchance all
+      //moves are bad. It will then pick the least bad of the bunch
+      if(score > highestScore){
+        highestScore = score; //if higher, this becomes the best play
+        index = i; //saves index to click box after all passes
+      }
+      currentBoard[availMoves[i]] = 0; //resets the move to test a different one
+    }
+     $('.boxes').children()[availMoves[index]].click(); //clicks the box that resulted in highest line scores
+
+
+};
+
+
+
   var updateBoard = function(item){ //updates the board
     currentBoard[item] = activePlayer; // sets played spot to 1 or 2 based off player
     var index = availMoves.indexOf(item); // finds the play just made
@@ -197,12 +218,12 @@ function initialize(){
 
 
   var checkWinner = function() { //takes current board setup and checks for winner
-      for(i=0; i<9; i+=3){ //checks for horizontal lines
+      for(var i=0; i<9; i+=3){ //checks for horizontal lines
           if(currentBoard[i] === currentBoard[i+1] && currentBoard[i] === currentBoard[i+2] && currentBoard[i] !== 0){
               return(currentBoard[i]);
           }
         }
-        for(i=0; i<3; i++){ //checks for horizontal lines
+        for(var j=0; j<3; j++){ //checks for horizontal lines
             if(currentBoard[i] === currentBoard[i+3] && currentBoard[i] === currentBoard[i+6] && currentBoard[i] !== 0){
                 return(currentBoard[i]); //return either 1 or 2 (for player that matched)
             }
